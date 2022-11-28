3752fcfa93c19235ae0386e333564d8f47397bc8
coraliefilion <116817945+coraliefilion@users.noreply.github.com>
2022-11-28 11:30:43 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 let Angle = 0
 servos.P0.setRange(10, 170)
 basic.forever(function () {
-    for (let index = 0; index <= 10; index++) {
+    for (let index = 0; index <= 16; index++) {
         Angle = index * 10 + 10
         servos.P0.setAngle(Angle)
         basic.pause(100)
+    }
+    if (Angle < 170) {
+        servos.P0.setRange(170, 5)
+        for (let index = 0; index <= 34; index++) {
+            Angle = index * 5 - 5
+            servos.P0.setAngle(Angle)
+            basic.pause(100)
+        }
     }
 })
